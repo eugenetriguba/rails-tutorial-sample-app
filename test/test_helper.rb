a7106523d3477dffe39634cb725b5ad378f84a81
Eugene Triguba <eugenetriguba@gmail.com>
2016-10-28 11:58:32 -0500

    Add account activation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,4 +28,4 @@ class ActionDispatch::IntegrationTest
                                           password: password,
                                           remember_me: remember_me } }
   end
-end                                  
+end
